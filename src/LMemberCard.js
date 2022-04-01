10e0695a86a130adb2c6f69e5622e0babb32fe24
mcyiyue <jngwiemalmsteen@gmail.com>
2022-04-01 15:51:36 +0800

    Update

--- src/LMemberCard.js
+++ src/LMemberCard.js
@@ -3,53 +3,59 @@ import React from 'react';
 
 class LMember extends React.Component {
 
-    constructor(){
+    constructor(props){ //must initialize props in parameter to assign it in variable
         super();
-        this.state = {
-            seeRobot: 'false',
-            invertedValue: 'true',
+        this.state  = {
+            invertedValue: 1,
+            seeRobot: 0,
             buttonText: 'See Robot Form'
         }
+        this.name = props.memberData.name;
+        this.title = props.memberData.title;
+        
     }
     
     onButtonClick = (event) => {
-        if (this.state.invertedValue ==='true'){    
+        
+        if (Boolean(parseInt(event.target.value))===true) {    
             this.setState({
-                    seeRobot : event.target.value,
-                    invertedValue: 'false',
+                    invertedValue : 0,
+                    seeRobot : 1,
                     buttonText: 'See Human Form'
                 }
-            )
+            );
         } else{
             this.setState({
-                seeRobot : event.target.value,
-                invertedValue: 'true',
+                invertedValue : 1,
+                seeRobot : 0,
                 buttonText: 'See Robot Form'
-            })
+            });
+        }
+        
+    }
+
+    setFilePath(){
+        console.log(this.props.memberData);
+        if(Boolean(this.state.seeRobot)){
+            return `https://robohash.org/${this.name}` //Set picture file path into robohash url if seeRobot value is 'true'
+        } else {
+            return `../img/${this.name}.jpg`; //Set picture file path into local image file if seeRobot value is not 'true'
         }
     }
 
     render(){
-    
-        const {name, title} = this.props.memberData;
-        let filePath='';
-
-        if(this.state.seeRobot==='true'){
-            filePath=`https://robohash.org/${name}` //Set picture file path into robohash url if seeRobot value is 'true'
-        } else {
-            filePath=`../img/${name}.jpg`; //Set picture file path into local image file if seeRobot value is not 'true'
-        }
+        
         return(
             <>  
                 <div className='col'>
                     <div className='tag'>
-                        <h6>{name}</h6>
-                        <p>{title}</p>
+                        <h6>{this.name}</h6>
+                        <p>{this.title}</p>
                     </div>
                     <div>
-                        <img src={filePath} className='' alt='No Photos'></img>
+                        <img src={this.setFilePath()} className='' alt='No Photos'></img>
                         <p>
-                            <button className='btn btn-primary'value={this.state.invertedValue} onClick={this.onButtonClick}>{this.state.buttonText}</button>
+                            <button className='btn btn-primary' value={this.state.invertedValue} onClick={this.onButtonClick}>{this.state.buttonText}</button>
                         </p>
                     </div>
                         
