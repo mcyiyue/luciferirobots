48ca724200dc9d65fe71361940d2fa984404eb02
mcyiyue <jngwiemalmsteen@gmail.com>
2022-04-07 08:21:51 +0800

    update

--- src/components/ScrollBox.js
+++ src/components/ScrollBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ScrollBox = (props) => {
     return (
-        <div className='container' style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
+        <div className='container' style={{overflowY: 'scroll', border: '1px solid black', height: '700px'}}>
                  <div className='row'>
                     {props.children}
                  </div>
--- src/containers/LMemberCard.js
+++ src/containers/LMemberCard.js
@@ -6,7 +6,6 @@ class LMember extends React.Component {
     constructor(){ //must initialize props in parameter to assign it in variable
         super();
         this.state  = {
-            invertedValue: 1,
             seeRobot: 0,
             buttonText: 'See Robot Form'
         }
@@ -14,45 +13,43 @@ class LMember extends React.Component {
     }
     
     onButtonClick = () => {
-        if (Boolean(parseInt(this.state.invertedValue))===true) {    
+        if (Boolean(this.state.seeRobot)) {    
             this.setState({
-                    invertedValue : 0,
-                    seeRobot : 1,
-                    buttonText: 'See Human Form'
+                    seeRobot : 0,
+                    buttonText: 'See Robot Form'
                 }
             );
         } else{
             this.setState({
-                invertedValue : 1,
-                seeRobot : 0,
-                buttonText: 'See Robot Form'
+                seeRobot : 1,
+                buttonText: 'See Human Form'
             });
         }
         
     }
 
-    setFilePath(){
+    setFilePath(name){
         if(Boolean(this.state.seeRobot)){
-            return `https://robohash.org/${this.name}` //Set picture file path into robohash url if seeRobot value is 'true'
+            return `https://robohash.org/${name}` //Set picture file path into robohash url if seeRobot value is 'true'
         } else {
-            return `../img/${this.name}.jpg`; //Set picture file path into local image file if seeRobot value is not 'true'
+            return `../img/${name}.jpg`; //Set picture file path into local image file if seeRobot value is not 'true'
         }
     }
 
     render(){
-        this.name = this.props.memberData.name;
-        this.title = this.props.memberData.title;
+        const {name, title} = this.props.memberData; //create variable name and title from object props memberdata
+        const {buttonText} = this.state;
         return(
             <>  
                 <div className='col'>
                     <div className='tag'>
-                        <h6>{this.name}</h6>
-                        <p>{this.title}</p>
+                        <h6>{name}</h6>
+                        <p>{title}</p>
                     </div>
                     <div>
-                        <img src={this.setFilePath()} className='' alt='No Photos' onClick={this.onButtonClick}></img>
+                        <img src={this.setFilePath(name)} className='' alt='No Photos' onClick={this.onButtonClick}></img>
                         <p>
-                            <button className='btn btn-primary' onClick={this.onButtonClick}>{this.state.buttonText}</button>
+                            <button className='btn btn-primary' onClick={this.onButtonClick}>{buttonText}</button>
                         </p>
                     </div>
                         
